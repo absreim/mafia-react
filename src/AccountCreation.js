11565f086663831b457f58b8997e89aefc787c76
Brook Li <absreim@users.noreply.github.com>
2018-06-19 04:30:59 -0400

    Refactored account login interface and created account creation interface.

--- src/AccountCreation.js
+++ src/AccountCreation.js
@@ -1,13 +1,18 @@
-/* User login interface. */
+/* Account creation interface. */
 
 import {Component} from "react"
 
-class AccountLogin extends Component{
+class AccountCreation extends Component {
     constructor(props){
         super(props)
-        this.state = {username: "", password: ""}
+        this.state = {
+            username: "",
+            password: "",
+            confirm: ""
+        }
         this.handleUsernameChange = this.handleUsernameChange.bind(this)
-        this.handlePasswordChange = this.handlePasswordChange.bind(this)
+        this.handlePasswordChange = this.handleUsernameChange.bind(this)
+        this.handleConfirmChange = this.handleConfirmChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
     }
     handleUsernameChange(event){
@@ -16,8 +21,12 @@ class AccountLogin extends Component{
     handlePasswordChange(event){
         this.setState({password: event.target.value})
     }
+    handleConfirmChange(event){
+        this.setState({confirm: event.target.value})
+    }
     handleSubmit(event){
-        this.props.submitCredentials(this.state.username, this.state.password)
+        // defer credential validation to component above
+        this.props.submitCrendentials(this.state.username, this.state.password, this.state.confirm)
         event.preventDefault()
     }
     render(){
@@ -27,6 +36,8 @@ class AccountLogin extends Component{
                 <input type="text" id="username" value={this.state.username} onChange={this.handleUsernameChange} />
                 <label for="password">Password</label>
                 <input type="text" id="password" value={this.state.password} onChange={this.handlePasswordChange} />
+                <label for="confirm">Confirm Password</label>
+                <input type="text" id="confirm" value={this.state.confirm} onChange={this.handleConfirmChange} />
                 <input type="submit" value="Submit" />
             </form>
         )
